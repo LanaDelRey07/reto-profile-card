1f9f83f63f61c320280f7bd8b4ff91d60ce41adb
ignacio cardozo <ignaciocardozomorales@gmail.com>
2022-04-07 11:22:58 -0400

    primer commit para js en main

---
+++ style/index.js
@@ -0,0 +1,8 @@
+function puntaje(PrimerP, SegundoP, finalP){
+    return PrimerP+SegundoP+finalP
+}
+
+debugger
+console.log("Hola a todos")
+
+console.log("Mi nota del semestre es: ",puntaje(20,25,10))
