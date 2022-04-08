a098ff0161625122da52ac76b4ee3f4fc8d560c0
ignacio cardozo <ignaciocardozomorales@gmail.com>
2022-04-08 13:31:54 -0400

    commit js del jueves

--- style/index.js
+++ style/index.js
@@ -1,8 +1,35 @@
-function puntaje(PrimerP, SegundoP, finalP){
-    return PrimerP+SegundoP+finalP
-}
+const elemento = document.getElementsByTagName('img')
+elemento[0].alt='profile'
 
-debugger
-console.log("Hola a todos")
+const indentificador = document.getElementById('ejemplo')
+indentificador.textContent = '700K'
+console.log(indentificador)
 
-console.log("Mi nota del semestre es: ",puntaje(20,25,10))
+const likes = document.querySelector('img')
+ likes.alt='dislikes'
+ console.log(likes)
+
+ const nodeListDeParrafos = document.querySelectorAll('p')
+ console.log(nodeListDeParrafos)
+
+ //creando
+
+ const profileCard = document.createElement('div')
+ profileCard.className = "profile-card"
+
+ const profileCardTop = document.createElement('div')
+ profileCardTop.className = "profile-card-top"
+
+ profileCard.appendChild(profileCardTop)
+
+ const app = document.querySelector('#app')
+ app.appendChild(profileCard)
+
+ const imagen = document.createElement('img')
+ imagen.src='./assets/image-victor.jpg'
+ imagen.alt='foto de perfil'
+ profileCardTop.appendChild(imagen)
+
+ const profileCardBottom = document.createElement('div ')
+ profileCardBottom.className = "profile-card-bottom"
+ profileCard.appendChild(profileCardBottom)
